8a3c7d5fd76fe189a7de04f8eb44710216f322c3
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-08-05 22:48:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-function decimalToBinary(num) {
-  //Write you code here
-	arr.sort((a, b) => a - b);
-  
-  // Initialize variables
-  let closestSum = arr[0] + arr[1] + arr[2];
-  
-  // Iterate through the array
-  for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
-    
-    while (left < right) {
-      const sum = arr[i] + arr[left] + arr[right];
-      
-      // Check if the current sum is closer to the target
-      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        closestSum = sum;
-      }
-      
-      if (sum < target) {
-        left++;
-      } else if (sum > target) {
-        right--;
-      } else {
-        // Found an exact match, so return the sum
-        return sum;
-      }
+    function decimalToBinary(decimal) {
+      return decimal.toString(2);
     }
-  }
-  
-  return closestSum;
-  
-}
-
-window.decimalToBinary = decimalToBinary;
+     
+    console.log(decimalToBinary(7));
+    console.log(decimalToBinary(10)); 
+    console.log(decimalToBinary(33)); 
